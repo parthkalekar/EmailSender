23b4470412bdf8a21651c1596122eaf852d661c4
Parth Kalekar <parthkalekar2002@gmail.com>
2024-05-04 13:22:21 +0530

    initial commit

---
+++ emailSender.js
@@ -0,0 +1,44 @@
+require("dotenv").config();
+const nodemailer = require("nodemailer");
+
+const emailSender = async (name, email, website, description, ipaddress) => {
+  const transporter = await nodemailer.createTransport({
+    host: process.env.SMTP_HOST,
+    port: process.env.SMTP_PORT,
+    secure: true,
+    auth: {
+      user: process.env.AUTH_USER,
+      pass: process.env.AUTH_PASS,
+    },
+  });
+
+  const newMessage = `
+    Name  :  ${name} \n
+    Email :  ${email}\n
+    Website :  ${website}\n
+    Description : ${description}\n
+    Date: ${new Date().toUTCString()}\n
+    IP: ${ipaddress}\n
+    `;
+
+  const details = {
+    from: "VERCEL PORTFOLIO",
+    to: process.env.TO_EMAIL,
+    subject: "PORFOLIO CONTACT",
+    text: `${newMessage}`,
+  };
+
+  try {
+    const checkMailSend = await transporter.sendMail(details);
+    if (checkMailSend) {
+      return true;
+    } else {
+      return false;
+    }
+  } catch (error) {
+    console.log(error);
+    return false;
+  }
+};
+
+module.exports = { emailSender };
---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const { emailSender } = require('./emailSender');
+require('dotenv').config();
+const app = express()
+const port = process.env.PORT || 5000
+
+app.use(express.json())
+
+app.get('/', (req, res) => res.send('Hello World!'));
+
+app.post('/contactme', async(req,res) =>{
+    try {
+
+        const {name,email,website,description} = req.body;
+        if(!(name && email && description)){
+            return res.status(406).json({result:false,message:"All Data Required...!"})
+        }
+
+        // const isMailSended = await emailSender(name,email,website,description, req.ip);
+        // console.log(isMailSended);
+
+        const isMailSended = await emailSender(name,email,website,description, req.ip)
+        if(!isMailSended){
+            return res.status(400).json({result:false,message:"Something went Wrong"})
+        }
+        res.status(200).json({result:true,message:"Message Registered Successfully"})
+        
+    } catch (error) {
+        res.status(500).json({result:true,message:"Internal Server Error...!",error:error.message})
+    }
+})
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
