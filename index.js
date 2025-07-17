d3abe553f85fc80dfe072e73699978407b9bf7d2
Parth Kalekar <parthkalekar2002@gmail.com>
2025-07-17 23:52:25 +0530

    origin allowed reconfigured

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const allowedOrigins = process.env.ALLOWED_ORIGINS.split(",")
 
 const corsOptions = {
   origin: function (origin, callback) {
-    if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+    if (!allowedOrigins.includes(origin)) {
       callback(null, true);
     } else {
       callback(new Error("Not allowed by CORS"));
